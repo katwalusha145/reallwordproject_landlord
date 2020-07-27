03dae10a5e6218ac5f647f966a1e8a7edcf83737
“[Ujjwal <[khatriujjwal0@gmail.com]>
2020-07-27 19:31:39 +0545

    Models changed

--- Rooms/models.py
+++ Rooms/models.py
@@ -5,13 +5,31 @@ class room(models.Model):
     Address=models.CharField(max_length=100)
     NumberOfRooms=models.IntegerField()
     price=models.FloatField()
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
     date = models.DateField(blank=True, null=True)
+>>>>>>> final
+>>>>>>> final
+=======
+>>>>>>> 8ec0bb0a8aadbc8d50544daee769579f9b28d94b
     im2 = models.ImageField(upload_to="site_media", blank="true")
     im3 = models.ImageField(upload_to="site_media", blank="true")
     im4 = models.ImageField(upload_to="site_media", blank="true")
     lender_contact=models.CharField(max_length=100)
 
 
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
     def __str__(self):
         return self.Address
 
+>>>>>>> final
+>>>>>>> final
+=======
+>>>>>>> 8ec0bb0a8aadbc8d50544daee769579f9b28d94b
