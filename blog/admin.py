4701c2c8d44c78e34fed7947777302ff10361e94
“[prakash <[rawalprakash722@gmail.com]>
2020-07-28 18:12:40 +0545

    updated blog

--- blog/admin.py
+++ blog/admin.py
@@ -8,16 +8,16 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
 
+
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'body', 'post', 'created_on', 'active')
     list_filter = ('active', 'created_on')
     search_fields = ('name', 'email', 'body')
     actions = ['approve_comments']
 
+
     def approve_comments(self, request, queryset):
         queryset.update(active=True)
-
-
 
 
 admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,6 @@ STATUS = (
     (0, "Draft"),
     (1, "Publish")
 )
-
-
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -15,10 +13,8 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-
     class Meta:
         ordering = ['-created_on']
-
     def __str__(self):
         return self.title
 
