8e040cddca0895601e0a376e0b711da5b00104fe
“[Ujjwal <[khatriujjwal0@gmail.com]>
2020-07-28 08:53:09 +0545

    forms added and models updated

---
+++ crud/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Position(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+class Employee(models.Model):
+    fullname = models.CharField(max_length=100)
+
+
+    review = models.CharField(max_length=100)
+
+
