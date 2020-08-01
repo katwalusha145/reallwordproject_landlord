d62295ae83cc7ecd612030b1d37650b9e4348541
“[prakash <[rawalprakash722@gmail.com]>
2020-08-01 20:31:06 +0545

    add views rooms

---
+++ Rooms/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from Rooms.models import room
+
+
+
+def show_room(request):
+    return render(request,'rooms.html')
+
+
+def show_new(request):
+    return render(request,'newrooms.html')
+
+
+def show_room(request):
+    ro=room.objects.all()
+    return render(request,'rooms.html',{'ro':ro})
